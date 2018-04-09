4af5dca0ed2bce65f45ca1ddb79ab12f4b4df0cd
hxz1993 <1432438153@qq.com>
2018-04-09 21:36:58 +0800

    移动端首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hu on 2018/4/9.
+ */
+$(function(){
+  //初始化scroll控件
+  mui('.mui-scroll-wrapper').scroll({
+    indicators: false
+  });
+
+  //轮播图
+  //获得slider插件对象
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+    interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+  });
+})
